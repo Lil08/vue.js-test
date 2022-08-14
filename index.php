50f71aa1d4e18d62d9477f798822e65fcec01f5d
Lil08 <maksakovaolga7@yanex.ru>
2022-08-14 23:41:38 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Test vue.js</title>
+</head>
+<body>
+
+<div class="container pt-5">
+    <div class="row mb-3">
+        <div class="col"><h1>Images</h1></div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="form-group">
+                <label for="search">Find your images</label>
+                <input type="text" class="form-control" id="search" >
+            </div>
+            <ul class="list-group">
+                <li class="list-group-item list-group-item-action">1</li>
+                <li class="list-group-item list-group-item-action">2</li>
+                <li class="list-group-item list-group-item-action">3</li>
+            </ul>
+            <div class="col-md-8">
+                <div class="col-md-5">
+                    <h2>Name</h2>
+                    <img src="" alt="">
+                </div>
+                <div class="col-md-7"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
